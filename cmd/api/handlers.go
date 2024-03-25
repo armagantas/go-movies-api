1851c33666ff8c954edf0fedcfdcd43e22b05db7
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-03-25 21:09:43 +0300

    Cors middleware has been added. Configurations are done. Some refactoring.

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -32,7 +32,7 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	var movies []models.Movie
 
-	rd, _ := time.Parse("2006-01-02", "1986,03-07")
+	rd, _ := time.Parse("2006-Jan-02", "1981-Jun-12")
 
 	highlander := models.Movie{
 		ID:          1,
@@ -47,7 +47,7 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 
 	movies = append(movies, highlander)
 
-	rd, _ = time.Parse("2006-01-02", "1981,06-12")
+	rd, _ = time.Parse("2006-Jan-02", "1981-Jun-12")
 
 	rotla := models.Movie{
 		ID:          2,
